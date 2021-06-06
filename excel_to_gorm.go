53e620b48729598ee83cdea06d07580c2805a8ad
Martin Lord <martinrlord@outlook.com>
2021-06-06 11:21:12 +0200

    catch blank constants

--- excel_to_gorm.go
+++ excel_to_gorm.go
@@ -168,7 +168,14 @@ func WorksheetToSlice(sh *xlsx.Sheet, model interface{}, params Params) (interfa
 					} else {
 						switch {
 						case tag.IsMapConst:
-							dbRecordPtr.Elem().Field(fldIx).Set(csv_to_gorm.StringToType(params.ConstMap[tag.ConstMapKey], fldType))
+							constString := params.ConstMap[tag.ConstMapKey]
+							// trying to convert empty strings to numbers in csv_to_gorm will bomb!
+							if constString == "" && fldType.Name() != "string" {
+								return fmt.Errorf("tag constant: " + tag.ConstMapKey + " missing for sheet:  " + sh.Name + ". ")
+							} else {
+								dbRecordPtr.Elem().Field(fldIx).Set(csv_to_gorm.StringToType(constString, fldType))
+							}
+
 						case tag.IsIntColsHead:
 							dbRecordPtr.Elem().Field(fldIx).Set(csv_to_gorm.StringToType(intColHdg, fldType))
 						case tag.IsIntColsValue:
